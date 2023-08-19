62b03fe38f31c9d199bc127032cf56a982822f1a
Deepak J M <140875610+deepakjm1@users.noreply.github.com>
2023-08-19 20:32:53 +0530

    Create code.ino

---
+++ code.ino
@@ -0,0 +1,21 @@
+//SECURITY ALARM USING IR SENSOR
+
+void setup() {
+  pinMode(2,INPUT);             // connect IR sensor"s output pin to D2 PIN of arduino
+  pinMode(12,OUTPUT);           // connect LED's POSITIVE pin to D2 PIN of arduino
+  pinMode(13,OUTPUT);           // connect BUZZER's POSITIVE pin to D2 PIN of arduino
+}
+
+void loop() {
+  if(digitalRead(2)==1)     
+ {
+   digitalWrite(12,0);
+   digitalWrite(13,0);
+ }
+ if(digitalRead(2)==0)
+ {
+   digitalWrite(12,1);
+   digitalWrite(13,1);
+ }
+
+}
